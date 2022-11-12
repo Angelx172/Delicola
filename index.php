dbc788544943158f2a24ad7abbd9437e95a0dc94
angel <palaciosangelx72@gmail.com>
2022-11-12 12:27:03 -0700

    Pagina web con CSS

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="estilos.css">
+    
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        include "header.html"
+    ?>
+    <div class="container col-6" style="margin-top: 3vh;">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+    </div>
+    <?php
+        include "footer.html"
+    ?>
+</body>
+</html>
