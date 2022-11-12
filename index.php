edc281e18cf6c5c4bd6e9488e869a18f2b2d7719
angel <palaciosangelx72@gmail.com>
2022-11-12 14:59:32 -0700

    Pagina principal y 2 mas

--- index.php
+++ index.php
@@ -14,24 +14,27 @@
     <?php
         include "header.html"
     ?>
-    <div class="container col-6" style="margin-top: 3vh;">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint deleniti nobis, ipsum facere esse quasi eaque voluptatum vel earum, beatae eum voluptas explicabo at tempore saepe labore nostrum! Cumque, unde.</p>
+    <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel" >
+  <div class="carousel-inner" style="max-height: 66vh;">
+    <div class="carousel-item active"">
+      <img src="70.jpg" class="d-block w-100" style="height: 66vh; alt="...">
     </div>
+    <div class="carousel-item">
+      <img src="colas.jpeg" class="d-block w-100" style="height: 66vh; alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="hold.jpg" class="d-block w-100" style="height: 80vh; alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
     <?php
         include "footer.html"
     ?>
